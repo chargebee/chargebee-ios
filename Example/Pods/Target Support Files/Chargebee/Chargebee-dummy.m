2b267e53da73d307ed68f4bb6ce1733b36021589
Prabu <meetprabu88@gmail.com>
2020-07-07 09:11:30 +0530

    chore: revert to initial

--- Example/Pods/Target Support Files/Chargebee/Chargebee-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Chargebee : NSObject
-@end
-@implementation PodsDummy_Chargebee
-@end
--- Example/Pods/Target Support Files/Pods-Chargebee_Example/Pods-Chargebee_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Chargebee_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Chargebee_Example
-@end
