12955028cad77ca4b6eb565b1d862b374b703242
Prabu <meetprabu88@gmail.com>
2020-07-09 15:17:36 +0530

    docs: add notes to fetch plan details

--- Example/Chargebee/ViewController.swift
+++ Example/Chargebee/ViewController.swift
@@ -10,28 +10,9 @@ import UIKit
 import Chargebee
 
 class ViewController: UIViewController {
-    @IBOutlet weak var resultLabel: UILabel!
 
-
-    @IBAction func tokenize() {
-        let card = CBCard(cardNumber: "4242424242424242", expiryMonth: "09", expiryYear: "29", cvc: "123")
-        let paymentDetail = CBPaymentDetail(type: "card", currencyCode: "USD", card: card)
-
-        CBManager().getTemporaryToken(paymentDetail: paymentDetail, completion: { s in
-            print("Final CB Token \(s)")
-            self.resultLabel.text = s
-        }, onError: {(error) in
-            print("Error\(error)")
-            self.resultLabel.text = error.localizedDescription
-        })
-
-//        CBManager().getPlan("cb-demo-no-trial") { res in
-//            print("Plan Details", res)
-//        }
-
-//        CBManager().getAddon("cbdemo_setuphelp") { res in
-//            print("Addon Details", res)
-//        }
-//        resultLabel.text = "Clicked value"
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
+
 }
