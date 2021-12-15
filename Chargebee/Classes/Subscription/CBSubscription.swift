18c4bfdf4ffacc1181520eb5ee301544d1ea1a3c
Imay <imayaselvan@gmail.com>
2021-12-15 16:01:18 +0530

    Added more fields for subscription

--- Chargebee/Classes/Subscription/CBSubscription.swift
+++ Chargebee/Classes/Subscription/CBSubscription.swift
@@ -16,12 +16,18 @@ public struct CBSubscriptionData: Codable {
     public let activatedAt: Double?
     public let status: String?
     public let planAmount: Double?
-    
-   
+    public let id: String?
+    public let currentTermEnd: Double?
+    public let currentTermStart: Double?
+
     enum CodingKeys: String, CodingKey  {
         case activatedAt = "activated_at"
         case status
+        case id
         case planAmount = "plan_amount"
+        case currentTermEnd = "current_term_end"
+        case currentTermStart = "current_term_start"
+
     }
 }
 
